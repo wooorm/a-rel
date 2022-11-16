fa28a3a456037b2235b670cdfeb968978247355b
Titus Wormer <tituswormer@gmail.com>
2022-11-16 17:28:07 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,20 +1,16 @@
-import assert from 'node:assert'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {aRel} from './index.js'
 
-test('aRel', function (t) {
-  t.ok(Array.isArray(aRel), 'should be an array')
+test('aRel', function () {
+  assert.ok(Array.isArray(aRel), 'should be an array')
 
-  t.doesNotThrow(function () {
-    let index = -1
-    while (++index < aRel.length) {
-      assert.strictEqual(
-        typeof aRel[index],
-        'string',
-        '`' + aRel[index] + '` should be a string'
-      )
-    }
-  }, 'should be a list of strings')
-
-  t.end()
+  let index = -1
+  while (++index < aRel.length) {
+    assert.strictEqual(
+      typeof aRel[index],
+      'string',
+      '`' + aRel[index] + '` should be a string'
+    )
+  }
 })
